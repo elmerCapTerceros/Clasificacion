0c9abab09171bc716c4d1d5132cdebdaa2d31946
samres <yososyam4@gmail.com>
2025-05-25 18:26:23 -0400

    add: view index

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/puntaje')
+def puntaje():
+    return render_template('puntaje.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
